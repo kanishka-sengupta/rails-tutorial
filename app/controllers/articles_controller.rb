c005cb9c9b540ca5403c2eb638f2a505e6ff8984
Kanishka Sengupta <kanishka.sengupta@zemosolabs.com>
2021-06-04 01:11:49 +0530

    New Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,17 @@ class ArticlesController < ApplicationController
         @articles=Article.all
     end
 
+    def new 
+
+    end
+
+    def create
+        #render plain: params[:article]
+        @article=Article.new(params.require(:article).permit(:title,:description))
+        #render plain: @article.inspect
+        @article.save
+        #redirect_to article_path(@article[:id])
+        #equivalent to
+        redirect_to :article        
+    end
 end
