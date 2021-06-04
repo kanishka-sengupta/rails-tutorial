3173389cd9b91e6824ca512a8507054454e96831
Kanishka Sengupta <kanishka.sengupta@zemosolabs.com>
2021-06-05 01:43:46 +0530

    Updating Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
         @article=Article.new
     end
 
+    def edit
+        @article=Article.find(params[:id])
+    end
+
     def create
         #render plain: params[:article]
         @article=Article.new(params.require(:article).permit(:title,:description))
@@ -24,5 +28,15 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end        
+    end
+
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice]="article was updated"
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 end
