94a04481e3e78f742388e3f9b3aef75773b62dcb
kanishka-sengupta <kanishka.sengupta@zemosolabs.com>
2021-06-15 00:50:26 +0530

    Login Logout Helper Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
 
     def show
-        @user=User.find(params[:id])
         #@articles=@user.articles
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user=User.find(params[:id])
     end
     
     def index
@@ -22,6 +21,7 @@ class UsersController < ApplicationController
     def create
         @user=User.new(user_params)
         if(@user.save)
+            session[:user_id]=@user.id
             flash[:notice]="Welcome to Alpha Blog, #{@user.username}. You have succesfully registered."
             redirect_to articles_path
         else
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user=User.find(params[:id])
         if @user.update(user_params)
             flash[:notice]="Your account was updated"
             redirect_to users_path
@@ -44,6 +43,10 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username,:email,:password)
+    end
+
+    def set_user
+        @user=User.find(params[:id])
     end
     
 end
