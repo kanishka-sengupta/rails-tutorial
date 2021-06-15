107dc77dc4844ccae556619b39670567b4f1a9df
kanishka-sengupta <kanishka.sengupta@zemosolabs.com>
2021-06-15 17:51:10 +0530

    Added Restriction Controller Level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
     def logged_in?
         !!current_user
     end
+
+    def require_user
+        if !logged_in?
+            flash[:notice]="You need to be logged in"
+            redirect_to login_path
+        end
+    end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :fetch_article , only: [:show,:edit,:update,:destroy]
+    before_action :require_user , except: [:show,:index]
+    before_action :require_same_user, only: [:edit,:update,:destroy]
     def show
         
     end
@@ -56,4 +58,12 @@ class ArticlesController < ApplicationController
     def permit_article_params
         params.require(:article).permit(:title,:description)
     end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] ="You can only edit or delete your own article"
+            redirect_to @article
+        end
+    end
+
 end
